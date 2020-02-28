3737339a85648e7c4825bb39fa7db0cbdb94a7c8
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-28 18:37:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,37 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-:weight, :handed, :complexion, :t_shirt_size,
-:wrist_size, :glove_size, :pant_length, :pant_width
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize (args)
-    args.each{|key, value|self.send("#{key}=",value)}
+  def initialize(args)
+    args.each{|key,value|self.send "#{key}=",value}
   end
-  #your code here
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+# :weight, :handed, :complexion, :t_shirt_size,
+# :wrist_size, :glove_size, :pant_length, :pant_width
+#
+#   def initialize (args)
+#     args.each{|key, value|self.send("#{key}=",value)}
+#   end
+#   #your code here
+# end
