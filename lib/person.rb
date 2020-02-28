943fff55002430696802ec522ae7eb278f87b613
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-28 18:39:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,11 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-  :weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
   :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(args)
-    args.each{|key,value|self.send "#{key}=",value}
-  end
-
+def initialize
+  args.each{|key, value|self.send "key=",value}
+end
 
 end
 
@@ -29,10 +28,9 @@ end
 #   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 # :weight, :handed, :complexion, :t_shirt_size,
 # :wrist_size, :glove_size, :pant_length, :pant_width
-# 
+#
 #   def initialize (args)
 #     args.each{|key, value|self.send("#{key}=",value)}
 #   end
 #   #your code here
 # end
-
