f057eec98a543ed155bf9298b42c51b587c16785
Jonathan Higger <jonathan.higger@gmail.com>
2020-02-28 18:30:07 +0000

    Automatically backed up by Learn

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+require 'require_all'
+require 'pry'
+# gem "rails"
---
+++ console.rb
@@ -0,0 +1,15 @@
+require 'require_all'
+require_all 'lib'
+require 'pry'
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+
+bob = Person.new(bob_attributes)
+bob.name       # => "Bob"
+bob.hair_color # => "Brown"
+
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+
+susan = Person.new(susan_attributes)
+susan.name      # => "Susan"
+susan.height    # => "5'11""
+susan.eye_color # => "Green"
